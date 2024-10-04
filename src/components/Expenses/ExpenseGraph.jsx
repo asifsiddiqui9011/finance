5f18e543c820413fb11bd65b80ac72b7b5ed39a2
Siddiqui Asif Ahmed <157271879+asifsiddiqui9011@users.noreply.github.com>
2024-10-04 13:38:41 +0530

    updated

--- src/components/Expenses/ExpenseGraph.jsx
+++ src/components/Expenses/ExpenseGraph.jsx
@@ -1,69 +1,79 @@
-// import CanvasJSReact from '@canvasjs/react-charts';
-// // var CanvasJS = CanvasJSReact.CanvasJS;
-// var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-// import { useContext, useState } from 'react';
-// import { FinanceContext } from '../../context/financeContext';
+
+import { useContext, useState } from 'react';
+import { FinanceContext } from '../../context/financeContext';
+import {Chart as ChartJs} from 'chart.js/auto'
+import {Bar,Doughnut,Line} from 'react-chartjs-2'
 
 
-// const ExpenseGraph = (props) => {
-//     const{allExpense}= useContext(FinanceContext)
+const ExpenseGraph = (props) => {
+    const{allExpense}= useContext(FinanceContext)
     
  
-//     const sortedData = allExpense.sort((a, b) => new Date(a.expense_date) - new Date(b.expense_date));
+    const sortedData = allExpense.sort((a, b) => new Date(a.expense_date) - new Date(b.expense_date));
 
   
   
-//    const dataPoints = [];
+   const dataPoints = [];
 
-//   sortedData.map((e)=>{
-//     if(sortedData){
-//       let date = `${e.expense_date}`
-//       let amount = `${e.amount}`
-//       dataPoints.push({ x: new Date(date), y: Number(amount)});
-//     }
+  sortedData.map((e)=>{
+    if(sortedData){
+      let date = `${e.expense_date}`
+      let amount = `${e.amount}`
+      dataPoints.push({ x: new Date(date), y: Number(amount)});
+    }
   
-//   })
+  })
 
   
   
 
-//   const[type,setType] = useState("bar")
+  const[type,setType] = useState("bar")
 
-//   const handleType = (e)=>{
-//     setType(e.target.value)
-//     console.log(type,"type",e.target.value)
-//   }
+  const handleType = (e)=>{
+    setType(e.target.value)
+    console.log(type,"type",e.target.value)
+  }
 
-//    const expenses = {
-//     animationEnabled: true,
-//     title:{
-//       text: "Monthly expense - 2024"
-//     },
-//     axisX: {
-//       valueFormatString: "DD/MM/YYYY"
-//     },
-//     axisY: {
-//       title: "expense (in INR)",
-//       prefix: "Rs"
-//     },
-//     data: [{
-//       yValueFormatString: "Rs#,###",
-//       xValueFormatString: "DD/MM/YYYY",
-//       type: `${type}`,
-//        dataPoints:dataPoints
+   const expenses = {
+    animationEnabled: true,
+    title:{
+      text: "Monthly expense - 2024"
+    },
+    axisX: {
+      valueFormatString: "DD/MM/YYYY"
+    },
+    axisY: {
+      title: "expense (in INR)",
+      prefix: "Rs"
+    },
+    data: [{
+      yValueFormatString: "Rs#,###",
+      xValueFormatString: "DD/MM/YYYY",
+      type: `${type}`,
+       dataPoints:dataPoints
      
-//     }]
-//   }
-//   return (
+    }]
+  }
+  return (
     
-//        <div className="budget-graph-container" style={props.style}>
-//         <select name="type" id="type" value={type} onChange={handleType}>
-//            <option value="bar">bar</option>
-//            <option value="spline">spline</option>
-//         </select>
-//               <CanvasJSChart options = {expenses} className="chart" id={'chart'}/>
-//       </div>
-//   )
-// }
+       <div className="budget-graph-container" style={props.style}>
+        <select name="type" id="type" value={type} onChange={handleType}>
+           <option value="bar">bar</option>
+           <option value="spline">spline</option>
+        </select>
+        <Bar
+        data={{
+            labels:allExpense.map((data)=>`${data.expense_date}`),
+            datasets:[
+                {
+                 label:"Budget",
+                 data:allExpense.map((data)=>data.amount),
+                 },
+            ],
+        }}
+      />      
+      </div>
+  )
+}
 
-// export default ExpenseGraph
+export default ExpenseGraph
